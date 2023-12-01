7fa62d401a328dc6946db210252a3e88e3b85d13
cinado <>
2023-12-01 01:54:25 +0100

    Initial commit. Setup of the repository for AOC

---
+++ target/debug/deps/part1-46b1ece5ff3bc084.d
@@ -0,0 +1,5 @@
+D:\Dokumente\AdventOfCode\2023\target\debug\deps\part1-46b1ece5ff3bc084.exe: day-01\src\bin\part1.rs
+
+D:\Dokumente\AdventOfCode\2023\target\debug\deps\part1-46b1ece5ff3bc084.d: day-01\src\bin\part1.rs
+
+day-01\src\bin\part1.rs:
