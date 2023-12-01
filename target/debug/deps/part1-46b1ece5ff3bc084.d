af8bd6a84268da9ba2e32c0f37a24821415a18ee
cinado <125228720+cinado@users.noreply.github.com>
2023-12-01 01:57:25 +0100

    Delete target directory

--- target/debug/deps/part1-46b1ece5ff3bc084.d
+++
@@ -1,5 +0,0 @@
-D:\Dokumente\AdventOfCode\2023\target\debug\deps\part1-46b1ece5ff3bc084.exe: day-01\src\bin\part1.rs
-
-D:\Dokumente\AdventOfCode\2023\target\debug\deps\part1-46b1ece5ff3bc084.d: day-01\src\bin\part1.rs
-
-day-01\src\bin\part1.rs:
